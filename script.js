b78e9f198e29563e058280b86d00ac32241cf39f
smartdon1212 <hammedabdulrahman99@gmail.com>
2025-07-19 18:54:40 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,56 @@
+function toggleMenu() {
+  document.getElementById("sidebar").classList.toggle("show");
+}
+
+function closeMenu() {
+  document.getElementById("sidebar").classList.remove("show");
+}
+
+    document.addEventListener("DOMContentLoaded", () => {
+ if (localStorage.getItem("smartdon-theme") === "dark") {
+      document.body.classList.add("dark-mode");}
+ });
+
+
+ //service.js     
+  // Auto scroll left to right
+    const container = document.getElementById('designGallery');
+    let scrollSpeed = 1;
+
+    function autoScroll() {
+      container.scrollLeft += scrollSpeed;
+      if (container.scrollLeft + container.clientWidth >= container.scrollWidth || container.scrollLeft <= 0) {
+        scrollSpeed = -scrollSpeed; // reverse scroll direction
+      }
+      requestAnimationFrame(autoScroll);
+    }
+
+    autoScroll();
+
+    // Mouse drag to scroll manually
+    let isDown = false;
+    let startX;
+    let scrollLeft;
+
+    container.addEventListener('mousedown', (e) => {
+      isDown = true;
+      container.classList.add('active');
+      startX = e.pageX - container.offsetLeft;
+      scrollLeft = container.scrollLeft;
+    });
+
+    container.addEventListener('mouseleave', () => {
+      isDown = false;
+    });
+
+    container.addEventListener('mouseup', () => {
+      isDown = false;
+    });
+
+    container.addEventListener('mousemove', (e) => {
+      if (!isDown) return;
+      e.preventDefault();
+      const x = e.pageX - container.offsetLeft;
+      const walk = (x - startX) * 2; //scroll-fast
+      container.scrollLeft = scrollLeft - walk;
+    });
